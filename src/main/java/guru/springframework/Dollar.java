457a212c15f39eebf11975432947d392e9fc2edd
KristiL <kristi.laur@mail.ee>
2018-12-31 22:09:32 +0200

    Dollar times method is now changed so that the object is immutable (end of video 17)

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -8,7 +8,13 @@ public class Dollar {
         this.amount = amount;
     }
 
-    void times(int multiplier) {
-        amount *= multiplier;
+    Dollar times(int multiplier) { // now multiplication original amount doesn't change,
+        // we return a new object of the calculation done on the original object
+        // when it was:
+        // void times (int multiplier) {
+        //  amount *= multiplier;
+        // }
+        // then the object was mutable and this can often be a source of errors
+        return new Dollar(amount * multiplier);
     }
 }
