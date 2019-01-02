4a6d7e1e42875b578b025214f7e0db5b38be50a7
KristiL13 <kristi.laur@mail.ee>
2019-01-02 16:53:50 +0200

    added conversion rate to work from bank, Pair as an object for conversion rates. added bank to reduce. new tests. (end of video 29)

--- src/main/java/guru/springframework/Sum.java
+++ src/main/java/guru/springframework/Sum.java
@@ -9,7 +9,8 @@ public class Sum implements Expression {
         this.addend = addend;
     }
 
-    public Money reduce(String toCurrency) { // will take 2 Money objects and reduce them to one object with sum operation
+    @Override
+    public Money reduce(Bank bank, String toCurrency) { // will take 2 Money objects and reduce them to one object with sum operation
         int amount = augend.amount + addend.amount;
         return new Money(amount, toCurrency);
     }
