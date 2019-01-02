8260e0aa2ee00e330ac3f872f9fbb95ba3ef5552
KristiL13 <kristi.laur@mail.ee>
2019-01-02 13:39:38 +0200

    moving times method from Dollar and Franc to Money (end of video 25)

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -6,14 +6,4 @@ public class Dollar extends Money {
         super(amount, currency);
     }
 
-    public Money times(int multiplier) { // now multiplication original amount doesn't change,
-        // we return a new object of the calculation done on the original object
-        // when it was:
-        // void times (int multiplier) {
-        //  amount *= multiplier;
-        // }
-        // then the object was mutable and this can often be a source of errors
-        return Money.dollar(amount * multiplier);
-    }
-
 }
